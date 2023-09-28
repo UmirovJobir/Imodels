58f89e92adfa25b71625091cc1956ce0136ddcec
Jobir Umirov <jobirumirovmoliya@gmail.com>
2023-09-28 23:00:04 +0500

    entrypoint

--- imodels/settings.py
+++ imodels/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['37.140.241.70', 'localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['jobir.uz', '37.140.241.70', 'localhost', '127.0.0.1',]
 
 
 # Application definition
