6a2ff30a533d2399b51928102ab39642c88f5a96
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-10-23 11:13:13 +0500

    docker nginx

---
+++ entrypoint.prod.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "Imodels" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+sleep 2
+
+
+echo "Apply database migrations"
+python3 manage.py makemigrations
+python3 manage.py migrate account
+python3 manage.py migrate shop
+python3 manage.py migrate
+
+exec "$@"
