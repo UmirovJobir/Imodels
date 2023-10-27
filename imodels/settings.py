7290722f8a0e20e67a33885539deb45503dfd3d5
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-10-27 18:52:26 +0500

    backend nginx

--- imodels/settings.py
+++ imodels/settings.py
@@ -172,7 +172,7 @@ STATIC_URL = 'backend/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = 'backend/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
