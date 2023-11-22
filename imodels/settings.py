720934794f1620139d70a812445c2eac6cdccdd2
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-11-22 10:27:23 +0500

    logs

--- imodels/settings.py
+++ imodels/settings.py
@@ -300,3 +300,35 @@ ADMIN_REORDER = (
         'django_summernote.Attachment',
     )},
 )
+
+
+LOGFILE_NAME = BASE_DIR / "log.txt"
+LOGFILE_SIZE = 2 * 1024 * 1024
+LOGFILE_COUNT = 1
+
+LOGGING = {
+    'version': 1,
+    "disable_existing_loggers":False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+        "logfile": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOGFILE_NAME,
+            "maxBytes": LOGFILE_SIZE,
+            "backupCount": LOGFILE_COUNT,
+            "formatter": "verbose",
+        },
+    },
+    "root":{
+        "handlers": ["console", "logfile"],
+        "level": "INFO",
+    },   
+}
