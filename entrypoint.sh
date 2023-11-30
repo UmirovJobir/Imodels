6f46f8de4eef46aa0d81b3ae60b15edf9003b042
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-11-30 18:23:18 +0500

    add description field in Product model

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate account
 python3 manage.py migrate shop
 python3 manage.py migrate
+gunicorn imodels.wsgi:application --bind 0.0.0.0:8000 --timeout 300
 
 # echo "Collect static files"
 # python3 manage.py collectstatic
