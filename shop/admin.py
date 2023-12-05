5b961001bf7a17d0404c50faf95539e4e33fa886
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-12-05 11:40:58 +0500

    comment sattic media url

--- shop/admin.py
+++ shop/admin.py
@@ -157,7 +157,7 @@ class ProductAdmin(TranslationAdmin, NestedModelAdmin, SummernoteModelAdmin):
     readonly_fields = ['id', 'price_table', 'first_image']
     raw_id_fields = ["category", "configurator", "description"]
     list_filter = [ProductFilter]
-    search_fields = ['title']
+    search_fields = ['title_uz', 'title_ru', 'title_en']
     inlines = [
         ProductImageInline,
         ProductVideoInline,
